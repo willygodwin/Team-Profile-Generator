f8d3bb04eb3a9f0e6e7aaf381236e3720ef56d63
willy <willygodwin47@gmail.com>
2021-01-28 12:14:33 +0800

    Manager Questions added to inquirer prompts

--- index.js
+++ index.js
@@ -16,7 +16,70 @@ let employees = [
     },
 ];
 
+
+// var prompt = function(question){
+//   return inquirer
+//     .prompt(question)
+//     .then(function(answers){
+//         if (condition === true){
+//           //loop back to first prompt again
+//           return prompt('question 1 data');
+//         }else{
+//           //ask second prompt
+//           return prompt('question 2 data');
+//         }
+//     });
+// }
+
+const teamQuestion = {
+                        type: 'list',
+                        name: 'team',
+                        message: 'Please add members to your team',
+                        choices: ['Engineer', 'Intern', 'Finished building my team'],
+                    }
+
 // inquirer 
+inquirer
+  .prompt([
+    {
+        type: 'input',
+        message: 'Please enter the team managers name?',
+        name: 'manager',
+    },
+    {
+        type: 'input',
+        message: 'Please enter the team managers employee ID?',
+        name: 'managerID',
+    },
+    {
+        type: 'input',
+        message: 'Please enter the team managers email?',
+        name: 'managerEmail',
+    },
+    {
+        type: 'input',
+        message: 'Please enter the team managers office number?',
+        name: 'managerOffice',
+    },
+    teamQuestion
+   
+  ])
+  .then((response) => {
+
+    switch (response.team) {
+        case 'Engineer':
+            return
+        case 'Intern':
+            return
+        case 'Finished building my team':
+            return
+        default:
+            return
+    }
+    
+    fs.writeFile("README.MD", getTemplate(response), (err) =>
+    err ? console.error(err) : console.log('Success!'))
+    });
 
 // keep asking the user to enter which employee type
 
