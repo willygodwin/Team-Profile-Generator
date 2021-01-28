4661864e11dec659d392b6186d04f6f38d3d7c05
willy <willygodwin47@gmail.com>
2021-01-28 14:59:34 +0800

    Added function to create HTML cards

--- index.js
+++ index.js
@@ -3,21 +3,22 @@ const inquirer = require('inquirer');
 const path = require('path');
 const Intern = require('./lib/intern');
 const Manager = require('./lib/manager');
+const Engineer = require('./lib/Engineer');
 const htmlHelpers = require('./src/utils/html');
-let employees = [
-    {
-        type: "Manager",
-        roomId: "12-A",
-        name: "Sam",
-        email: "sam@sam.com",
-    },
-    {
-        type: "Engineer",
-        github: "sam-ngu",
-        name: "Sam",
-        email: "sam@sam.com",
-    },
-];
+// let employees = [
+//     {
+//         type: "Manager",
+//         roomId: "12-A",
+//         name: "Sam",
+//         email: "sam@sam.com",
+//     },
+//     {
+//         type: "Engineer",
+//         github: "sam-ngu",
+//         name: "Sam",
+//         email: "sam@sam.com",
+//     },
+// ];
 
 
 // function prompt(question){
@@ -139,10 +140,10 @@ function promptQuestions(questions){
     if(response.isManager){
         let manager = new Manager(response.name, response.email, response.id, response.office);
         myTeam.push(manager);
-    } else if (isEngineer){
+    } else if (response.isEngineer){
         let engineer = new Engineer(response.name, response.email, response.id, response.github);
         myTeam.push(engineer);
-    } else if (isIntern){
+    } else if (response.isIntern){
         let intern = new Intern(response.name, response.email, response.id, response.school);
         myTeam.push(intern);
     }
@@ -155,23 +156,50 @@ function promptQuestions(questions){
             promptQuestions(internQuestions)
             break;
         case 'Finished building my team':
+            console.log(myTeam);
+            addCards(myTeam) 
+            createHTML()
             return
         default:
+            console.log(myTeam);
+            addCards(myTeam) 
             return
     }
     });
+
+
 }
 
 promptQuestions(managerQuestions)
-console.log(myTeam);
+
 
 // keep asking the user to enter which employee type
 
 // ask question specific to emmployee type
 // ;
+function addCards(team){
+    fs.writeFile("cards.html", '', (err) =>
+        err ? console.log(err) : console.log('Success!'))
+    for (const objClass of team) {
+        const htmlPath = path.join(__dirname, "src", "html-templates", "card.html");
+        const layout = fs.readFileSync(htmlPath, "utf-8");
+        const newHTML = htmlHelpers.injectCode(layout, '{{ name }}', objClass.getName());
+        newHTML = htmlHelpers.injectCode(newHTML, '{{ role }}', objClass.getRole());
+        newHTML = htmlHelpers.injectCode(newHTML, '{{ id }}', objClass.getID());
+        newHTML = htmlHelpers.injectCode(newHTML, '{{ email }}', objClass.getEmail());
 
+        fs.appendFile('cards.html', newHTML, function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+          });
+        
+    }
+}
 
-
-const htmlPath = path.join(__dirname, "src", "html-templates", "template.html");
-const layout = fs.readFileSync(htmlPath, "utf-8");
-htmlHelpers.injectCode(layout, '{{ code_injection }}', JSON.stringify(employees));
+function createHTML() {
+    const htmlPath = path.join(__dirname, "src", "html-templates", "template.html");
+    const layout = fs.readFileSync(htmlPath, "utf-8");
+    const newHTML = htmlHelpers.injectCode(layout, '{{ code_injection }}', 'cards.html');
+    fs.writeFile("index.html", newHTML, (err) =>
+        err ? console.log(err) : console.log('Success!'))
+}
